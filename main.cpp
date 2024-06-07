b852c316400c06881aa0fa2372d36f16da962f2c
azazolik132 <it-arturkryvosheiev@stredniskola.net>
2024-06-07 08:54:17 +0200

    Funkce trefil nebo ne

--- main.cpp
+++ main.cpp
@@ -10,6 +10,11 @@ int main(int argc, char *argv[]){
     tajneCislo = rand()%5+1; // nahodne od 1 do 5
     cout << "Zadej svuj typ na tajne cislo: ";
     cin >> hracovoCislo;
+    if(tajneCislo == hracovoCislo) {
+        cout << " Trefil si!" <<endl;
+    }
+    else cout << "Netrefil si :(" <<endl ;
+    cout << "Tajne cislo bylo: " << tajneCislo;
     return 0;
     
 }
