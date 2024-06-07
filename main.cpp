8f7c0ed8d99dc596bfe42cf6c08cf56fe25ce345
azazolik132 <it-arturkryvosheiev@stredniskola.net>
2024-06-07 08:31:06 +0200

    Inicializovan generator nahodnych cisel

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include "iostream"
+#include "ctime"
 
 using namespace std;
 // hadani tajneho cisla
 int main(int argc, char *argv[]){
+    srand(time(0));
     int tajneCislo;
     int hracovoCislo;
     tajneCislo = rand()%5+1; // nahodne od 1 do 5
     cout << "Zadej svuj typ na tajne cislo: ";
     cin >> hracovoCislo;
     return 0;
+    
 }
